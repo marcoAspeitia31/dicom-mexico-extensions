f9794f181308e9fa35aed86bbd9bd3f21d208ae8
Marco Aspeitia <maspeitia.31@gmail.com>
2022-12-28 12:41:15 -0600

    merge news-block into featured-001-posts-metaboxes

--- admin/blocks/news/build/index.asset.php
+++ admin/blocks/news/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b8d3e6f7f63675b2183e');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3e725d357b8d76a12169');
