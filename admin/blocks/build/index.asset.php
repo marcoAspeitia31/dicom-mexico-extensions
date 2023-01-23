6e8dc45e3133e9ef910e4de57958734f64d6a576
Marco Aspeitia <maspeitia.31@gmail.com>
2023-01-23 00:03:16 -0600

    build to staging

--- admin/blocks/build/index.asset.php
+++ admin/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '44d91fdd7294df158664');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '136bdabb3e07a66135c0');
