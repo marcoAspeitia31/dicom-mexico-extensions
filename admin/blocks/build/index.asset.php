16dc9aa9c9fa049b1254ab16aecf63c107c4c4da
Marco Aspeitia <maspeitia.31@gmail.com>
2023-02-05 21:54:08 -0600

    build blocks

--- admin/blocks/build/index.asset.php
+++ admin/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0e0d5cbbbe04e50284e6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f1122c3546b4bc6f3abc');
